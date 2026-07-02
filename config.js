0999b14380db77f1937740e0b92fbc7f6547ed0d
damstrivandrum <aajalms@gmail.com>
2026-07-03 00:43:04 +0530

    Update exam start and end times to July 3, 2026

--- config.js
+++ config.js
@@ -17,8 +17,8 @@ window.MARKS_WRONG = -1;
 window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed window (IST)
-window.EXAM_START_IST = "2026-07-01T10:00:00+05:30";
-window.EXAM_END_IST   = "2026-07-01T13:15:00+05:30";
+window.EXAM_START_IST = "2026-07-03T00:15:00+05:30";
+window.EXAM_END_IST   = "2026-07-03T13:15:00+05:30";
 
 // Parsed milliseconds (used by pages)
 window.EXAM_START_MS = Date.parse(window.EXAM_START_IST);
