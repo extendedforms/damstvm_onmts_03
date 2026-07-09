d40384a6536e4bcf708f97bdd28846f86d1a6bee
damstrivandrum <aajalms@gmail.com>
2026-07-09 12:47:12 +0530

    Update question and options in questions.js

--- questions.js
+++ questions.js
@@ -1,17 +1,16 @@
 // Format: { id, image?, question, options[], answerIndex, correctAnswerText }
 window.questions = [
   {
-    id: 1,
-    image: "images/ScreenRecording_07-09-2026 11-58-33_1.mov",
-    question: "A 7-year-old unimmunized child is brought with high-grade fever, cough, coryza, conjunctival congestion, and marked photophobia. On oral examination, multiple tiny bluish-white lesions surrounded by an erythematous halo are seen on the buccal mucosa opposite the molar teeth. Two days later, a maculopapular rash appears behind the ears and spreads downward.\nWhich of the following is the most likely diagnosis?",
+    id: 11,
+    image: "",
+    question: "A video shows a patient walking with a wide-based, unsteady gait. Which structure is involved?",
     options: [
-      "Measles",
-      "Rubella",
-      "Scarlet fever",
-      "Varicella"
+      "Basal ganglia",
+      "Vestibular nerve",
+      "Cerebellar vermis",
+      "Dorsal column"
     ],
-    answerIndex: 0,
-    correctAnswerText: "Measles"
-   }
+    answerIndex: 2,
+    correctAnswerText: "Cerebellar vermis"
+  }
 ];
-    
